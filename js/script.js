72ec184d160c78ce00bd8b4895ac8ad5aa6099d0
pierlucamario_dussich <pierluca.dussich@gmail.com>
2020-11-17 17:03:32 +0100

    slider-vue

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 const imagesArray = [
-{ name: mountain ,
-  src: 'img/immagine1.jpg'
+{ name: 'mountain' ,
+  src: 'img/immagine1.jpg',
 },
-{ name: waterfall,
-  src: 'img/immagine2.jpg'
+{ name: 'waterfall',
+  src: 'img/immagine2.jpg',
 },
-{ name: meadows,
-  src: 'img/immagine3.jpg'
+{ name: 'meadows',
+  src: 'img/immagine3.jpg',
 },
-{ name: lake,
-  src: 'img/immagine4.jpg'
-}];
+{ name: 'lake',
+  src: 'img/immagine4.jpg',
+},
+];
 
 
 const sliderApp = new Vue ({
@@ -19,4 +20,5 @@ const sliderApp = new Vue ({
      img: imagesArray.map( e => e.src),
      imagesIndexArray: 0,
    }
+
 });
